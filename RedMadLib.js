6fbad96d06316ec2c0df4a2340aa74467249db9b
meganmcgeee <meganpmcgee@gmail.com>
2017-03-19 11:39:31 -0700

    Initial commit

---
+++ RedMadLib.js
@@ -0,0 +1,36 @@
+Little	|hoodColor|	Riding	Hood	was	walking	through	|country|	one	her	way	
+											(color)																																																																							(location)
+to	visit	her	sick	|relative|	was	home	in	bed	with		|illness| .	
+																				(relative)																																																																					(disease)
+Little |hoodColor|	Riding	Hood	had	a	basket	filled	with	____________________		
+												(same	color)																																																																																(food)
+and	_____________________		and	_____________________	that	she	had	made	herself.	
+													(different	food)																				(beverage)
+Her	mother	told	her	not	to	leave	the	path	or	talk	to	_______________________.	
+																																																																																																				(something	scary)									
+But	Little |hoodColor|	Riding	Hood	didn’t	listen	to	her	mother.	She	met	the	
+																					(same	color)
+Big,	Bad,	|smallFurryAnimal|	and	told	him	exactly	where	she	was	going.	
+																							(small	furry	animal)
+The	Big,	Bad	|smallFurryAnimal|	raced	ahead	to	|smallFurryAnimal|'s	
+																								(same	small	furry	animal)																										(same	relative)
+house	and	tossed	|relative|	in	the	closet.	Then	he	got	into	a	|clothing|
+																																			 (same	relative)																																																																				(type	of	clothing)																																																								
+and	slipped	into	bed.	
+When	Little	|hoodColor| Riding	Hood	arrived	she	said,	“|relative|	what	big
+																							(same	color)																																																									(same	relative)
+_______________________	you	have.	The	better	to	___________________	you	with	my	dear.	
+													(body	part)																																																(one	of	the	5	senses)
+_________________________	what	big	|bodyPart|	you	have.	The	better	to
+					(same	relative)																										(another	body	part)
+|oneOfSenses|	you with	my	dear	and	______________________	what	big	
+				(another	sense)																																																														(same	relative)
+___________________________	you	have	the	better	to	_______________________	my	dear.	
+				(third body	part)																																																								(a	different	sense)
+And	then	_________________________	jumped	out	of	bed	and	chased	Little	_____________
+																						(relative)																																																																																												(same	color)
+Riding	Hood	all	around	the	cottage	until	__________________________		showed	up	and	said
+																																																																													(famous	powerful	person)
+______________________________________.		The	big,	bad	|smallFurryAnimal|	screamed	and	ran	away	
+(something	that	person	might	say)																							
+and	they	all	lived	Happily	Ever	After.		
